b7e883b89eaf3db48a70d901e373d7c3729b4834
Immanuel Fernandes <152952734+Immanuel-Fernandes@users.noreply.github.com>
2024-07-18 18:31:40 +0530

    Update disha.py

--- disha.py
+++ disha.py
@@ -8,7 +8,7 @@ model = T5ForConditionalGeneration.from_pretrained(model_name)
 
 def summarize_text(input_text):
     inputs = tokenizer.encode("summarize: " + input_text, return_tensors="pt", max_length=512, truncation=True)
-    summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary_ids = model.generate(inputs, max_length=50, min_length=10, length_penalty=2.0, num_beams=4, early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
 
